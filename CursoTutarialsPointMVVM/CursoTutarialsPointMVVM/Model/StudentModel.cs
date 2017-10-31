6454504c23261c7e0d04c0913aa6311d8cee8d95
Fagner <meurerfagner@hotmail.com>
2017-10-31 00:02:15 -0200

    Introdução da criação do MVVM

---
+++ CursoTutarialsPointMVVM/CursoTutarialsPointMVVM/Model/StudentModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace CursoTutarialsPointMVVM.Model
+{
+    class StudentModel { };
+    class Student : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    RaisePropertChanged("FirstName");
+                    RaisePropertChanged("LastName");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    RaisePropertChanged("LastName");
+                    RaisePropertChanged("FirstName");
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return firstName + " " + lastName;
+            }
+        }
+        private void RaisePropertChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ CursoTutarialsPointMVVM/CursoTutarialsPointMVVM/View/StudentView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace CursoTutarialsPointMVVM.View
+{
+    /// <summary>
+    /// Interação lógica para StudentView.xam
+    /// </summary>
+    public partial class StudentView : UserControl
+    {
+        public StudentView()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModel.StudentViewModel();
+        }
+    }
+}
